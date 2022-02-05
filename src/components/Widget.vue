7c999e15b6bcd1cf3fa7234c671cff398c77fe0a
AdRuslan <russian.adig@mail.ru>
2022-02-05 14:12:08 +0300

    fix

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -20,18 +20,11 @@
       <v-list-item three-line>
         <v-list-item-content>
           <v-list-item-title class="text-h5 mb-3">
-            Количество <br> объявлений:
+            Количество объявлений:
           </v-list-item-title>
           <v-list-item-subtitle>Автомобили: {{adsQuantity.cars}}</v-list-item-subtitle>
           <v-list-item-subtitle>Квартиры: {{adsQuantity.apartments}}</v-list-item-subtitle>
         </v-list-item-content>
-
-        <v-img
-            tile
-            max-width="150"
-            class="rounded-lg my-5"
-            src="/img/widget_img.jpg"
-        ></v-img>
       </v-list-item>
 
     </v-card>
